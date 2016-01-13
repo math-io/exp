1ecfe1ee0dc303c2ebb2b0471f0328106eb915cd
kgryte <kgryte@gmail.com>
2016-01-12 16:16:17 -0800

    Add files

---
+++ test/test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// MODULES //
+
+var test = require( 'tape' );
+var abs = require( 'math-abs' );
+var exp = require( './../lib' );
+
+
+// TESTS //
+
+test( 'main export is a function', function test( t ) {
+	t.ok( typeof exp === 'function', 'main export is a function' );
+	t.end();
+});
+
+test( 'the function evaluates the natural exponential function', function test( t ) {
+	var delta;
+
+	// Compared against output from Julia...
+	delta = abs( exp( 4 ) - 54.598150033144236);
+	t.ok( delta < 1e-14, 'equals ~54.598150033144236' );
+
+	delta = abs( exp( -9 ) - 1.2340980408667956e-4 );
+	t.ok( delta < 1e-14, 'equals ~1.2340980408667956e-4' );
+
+	t.equal( exp( 0 ), 1, 'equals 1' );
+	t.end();
+});
+
+test( 'the function returns `0` if provided a `-infinity`', function test( t ) {
+	var val = exp( Number.NEGATIVE_INFINITY );
+	t.equal( val, 0, 'returns 0' );
+	t.end();
+});
+
+test( 'the function returns `+infinity` if provided a `+infinity`', function test( t ) {
+	var val = exp( Number.POSITIVE_INFINITY );
+	t.equal( val, Number.POSITIVE_INFINITY, 'returns +infinity' );
+	t.end();
+});
+
+test( 'the function returns `NaN` if provided a `NaN`', function test( t ) {
+	var val = exp( NaN );
+	t.ok( val !== val, 'returns NaN' );
+	t.end();
+});
